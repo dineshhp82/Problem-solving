07f73f61d7c2bf154030b9763c6701f66cbe3d75
Dinesh Sharma <dineshhp82@hotmail.com>
2020-08-18 14:10:10 +0530

    More folder add

--- ProblemSolving/KadaneAlgorithm.cs
+++ ProblemSolving/KadaneAlgorithm.cs
@@ -12,7 +12,7 @@ namespace ProblemSolving
     {
         //int[] array={4,3,-2,2,3,1,-2,-3,4,2,-6,-3,-1,3,1,2}
         public void SolveProblem(int[] array)
-        {       
+        {
             int max_so_far = array[0]; //var replace only value less then current max value
             int max_ending_here = 0; //var is used to store sequence value sum and reset if it's -ve
 
@@ -27,7 +27,7 @@ namespace ProblemSolving
                 if (max_so_far < max_ending_here)
                 {
                     max_so_far = max_ending_here;
-                    
+
                     start_index = s;
                     end_index = i;
                 }
@@ -38,7 +38,7 @@ namespace ProblemSolving
                     s = i + 1;
                 }
             }
-            Console.WriteLine("Start Index =>"+start_index);
+            Console.WriteLine("Start Index =>" + start_index);
             Console.WriteLine("End Index =>" + end_index);
             Console.WriteLine("max value =>" + max_so_far);
         }
